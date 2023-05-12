205134c543f43f18604d5b1842fc29639574f580
ikhyeons <skantrkwl789@naver.com>
2023-05-13 00:54:53 +0900

    Update myPage
    헤더 생성

--- pages/myPage.tsx
+++ pages/myPage.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {ReactElement} from 'react';
 import Layout from '@/components/layout/layout';
 import MyPageCtg from '@/components/myPage/myPageCtg';
+import Header from '@/components/myPage/header';
 
 function MyPage() {
 	return (
 		<>
-			<header>나의 분양</header>
+			<Header />
 			<MyPageCtg />
 			<ul>
 				<li>내용1</li>
