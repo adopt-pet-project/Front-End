033aad42223375f405069e7258ebf9574f843fca
ikhyeons <skantrkwl789@naver.com>
2023-05-16 23:17:15 +0900

    Update myPage
    프로필 사진 미리보기 설정

--- components/myPage/profile.tsx
+++ components/myPage/profile.tsx
@@ -3,8 +3,17 @@ import {useEffect, useRef, useState} from 'react';
 function Profile() {
 	const [isModify, setIsModify] = useState(false);
 	const inputRef = useRef<HTMLInputElement>(null);
+	const inputImgRef = useRef<HTMLInputElement>(null);
 	const [name, setName] = useState('John Doe');
+	const [newName, setNewName] = useState('John Doe');
 
+	const dummyImg =
+		'https://mblogthumb-phinf.pstatic.net/MjAxNjExMjJfMjEx/MDAxNDc5NzQ0MDAzOTQy.-ax_EfCGWODogkXHIuDpovF5XHfaYi_s8EtRVWEjYXQg.R4kQWRtNC7pNxF03-aKWylWpGoRgE7vGDeagJm7Sgk0g.PNG.outdoor-interlaken/%EC%8A%A4%EC%9C%84%EC%8A%A4_%EC%97%AC%ED%96%89%ED%95%98%EA%B8%B0_%EC%A2%8B%EC%9D%80_%EA%B3%84%EC%A0%88_christofs70.png?type=w800';
+	const [currentImg, setCurrentImg] = useState(dummyImg);
+
+	useEffect(() => {
+		setName(name);
+	}, []);
 	useEffect(() => {
 		inputRef.current?.focus();
 	}, [isModify]);
@@ -14,14 +23,31 @@ function Profile() {
 				<div className={styles.imgWrap}>
 					<img
 						className={styles.profileImg}
-						src="https://mblogthumb-phinf.pstatic.net/MjAxNjExMjJfMjEx/MDAxNDc5NzQ0MDAzOTQy.-ax_EfCGWODogkXHIuDpovF5XHfaYi_s8EtRVWEjYXQg.R4kQWRtNC7pNxF03-aKWylWpGoRgE7vGDeagJm7Sgk0g.PNG.outdoor-interlaken/%EC%8A%A4%EC%9C%84%EC%8A%A4_%EC%97%AC%ED%96%89%ED%95%98%EA%B8%B0_%EC%A2%8B%EC%9D%80_%EA%B3%84%EC%A0%88_christofs70.png?type=w800"
+						src={currentImg}
 						alt="프로필 이미지"
 					/>
 					<label htmlFor="profileInput" className={styles.hoverWrap}>
 						<div className={styles.imageBtn}>수정</div>
 					</label>
 
-					<input id="profileInput" className={styles.fileInput} type="file" />
+					<input
+						ref={inputImgRef}
+						onInput={e => {
+							console.log(e.currentTarget.files);
+							if (inputImgRef.current) {
+								const file = inputImgRef?.current?.files![0];
+								const reader = new FileReader();
+								reader.readAsDataURL(file);
+								reader.onloadend = () => {
+									setCurrentImg(reader.result as string);
+								};
+							}
+						}}
+						id="profileInput"
+						accept="image/*"
+						className={styles.fileInput}
+						type="file"
+					/>
 				</div>
 
 				<div className={styles.profileInfo}>
@@ -32,14 +58,23 @@ function Profile() {
 					>
 						{isModify ? (
 							<input
+								value={newName}
 								ref={inputRef}
 								type="text"
 								onChange={e => {
-									setName(e.target.value);
+									setNewName(e.target.value);
+								}}
+								onBlur={() => {
+									setNewName(name);
+									setIsModify(false);
 								}}
 								onKeyUp={e => {
 									console.log(e.code);
-									if (e.code === 'Enter' || e.code === 'Escape') {
+									if (e.code === 'Enter') {
+										setName(newName);
+										setIsModify(false);
+									} else if (e.code === 'Escape') {
+										setNewName(name);
 										setIsModify(false);
 									}
 								}}
