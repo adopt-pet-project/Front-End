8170365a355757e83c7a04cedc6226de5a4f1907
ikhyeons <skantrkwl789@naver.com>
2023-05-18 18:27:25 +0900

    Update alarm
    알람 모달 생성 중

--- components/header/alarmBox.tsx
+++ components/header/alarmBox.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import styles from '@/styles/components/header/alarmBox.module.scss';
 
-function AlarmBox() {
-	return <div>alarm</div>;
+function AlarmBox({
+	setIsAlarmBoxOn,
+}: {
+	setIsAlarmBoxOn: (status: boolean) => void;
+}) {
+	return <div className={styles.box}>alarm</div>;
 }
 
 export default AlarmBox;
