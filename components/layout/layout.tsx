868e0c7d6366be49ed9ebb745eba5912690c7c70
ikhyeons <skantrkwl789@naver.com>
2023-05-21 02:29:45 +0900

    Update layout
    기존 _app에서 recoil과 react-query provider가 있었는데, layout까지 provider가 안닫아서
    provider를 layout으로 이동

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,20 +1,28 @@
 import {ReactElement, RefObject, useRef} from 'react';
 import Aside from '../aside/aside';
 import Header from '../header/header';
+import {RecoilRoot, useRecoilState} from 'recoil';
+import {QueryClient, QueryClientProvider} from 'react-query';
+
+const queryClient = new QueryClient();
 
 export default function Layout({children}: {children: ReactElement}) {
 	const asideRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
 	const containerRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
 
 	return (
-		<div className="layout">
-			<Header asideRef={asideRef} containerRef={containerRef} />
-			<Aside asideRef={asideRef} />
-			<main className="main">
-				<div ref={containerRef} className="container">
-					{children}
+		<QueryClientProvider client={queryClient}>
+			<RecoilRoot>
+				<div className="layout">
+					<Header asideRef={asideRef} containerRef={containerRef} />
+					<Aside asideRef={asideRef} />
+					<main className="main">
+						<div ref={containerRef} className="container">
+							{children}
+						</div>
+					</main>
 				</div>
-			</main>
-		</div>
+			</RecoilRoot>
+		</QueryClientProvider>
 	);
 }
