8a0a8194a401eaed16412c84053aabef6dbf94b6
ikhyeons <skantrkwl789@naver.com>
2023-05-22 05:14:19 +0900

    Update chatting
    채팅창 아래로 내리기 구현

--- components/header/alarm/alarmCard/chat/input.tsx
+++ components/header/alarm/alarmCard/chat/input.tsx
@@ -1,8 +1,11 @@
 import React, {useRef} from 'react';
 import styles from '@/styles/components/header/alarm/alarmCard/chat/input.module.scss';
+import {useRecoilState} from 'recoil';
+import {AgaraChat} from '@/utils/recoil/recoilStore';
 
 function Input() {
 	const textarea = useRef<HTMLTextAreaElement>(null);
+	const [chatData, setChatData] = useRecoilState(AgaraChat);
 
 	const handleResizeHeight = () => {
 		if (textarea && textarea.current) {
@@ -14,7 +17,38 @@ function Input() {
 		<div className={styles.inputWrap}>
 			<img className={styles.icon} src="/icon/picture.svg" alt="" />
 			<img className={styles.icon} src="/icon/location.svg" alt="" />
-			<textarea rows={1} onChange={handleResizeHeight} ref={textarea} />
+			<textarea
+				onKeyUp={e => {
+					if (
+						e.code === 'Enter' &&
+						e.currentTarget.value !== '\n' &&
+						e.currentTarget.value !== ''
+					) {
+						e.currentTarget.value = '';
+						setChatData(prev => [
+							...prev,
+							{
+								id: 5,
+								type: 'text',
+								chatContents: '분양 받으시려면 여기로 오세욘',
+								date: '2023. 5. 7 16:40',
+								checked: false,
+								isMy: true,
+							},
+						]);
+					}
+					handleResizeHeight();
+				}}
+				onInput={e => {
+					if (e.currentTarget.value === '\n') {
+						e.currentTarget.value = '';
+					}
+					handleResizeHeight();
+				}}
+				rows={1}
+				onChange={handleResizeHeight}
+				ref={textarea}
+			/>
 			<img className={styles.icon} src="/icon/send.svg" alt="" />
 		</div>
 	);
