cce88a535ed37189ddf8f6e8851ca8d20a4a5654
ikhyeons <skantrkwl789@naver.com>
2023-05-22 17:10:23 +0900

    Update chatting
    채팅 인풋 이미지, 위치

--- components/header/alarm/alarmCard/chat/input.tsx
+++ components/header/alarm/alarmCard/chat/input.tsx
@@ -1,11 +1,16 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import styles from '@/styles/components/header/alarm/alarmCard/chat/input.module.scss';
 import {useRecoilState} from 'recoil';
 import {AgaraChat} from '@/utils/recoil/recoilStore';
+import InputImgLine from './inputImgLine';
+import InputLocation from './inputLocation';
 
 function Input() {
 	const textarea = useRef<HTMLTextAreaElement>(null);
 	const [chatData, setChatData] = useRecoilState(AgaraChat);
+	const [inputValue, setInputValue] = useState('');
+	const [isImgInput, setIsImgInput] = useState(false);
+	const [isLocationInput, setIsLocationInput] = useState(false);
 
 	const handleResizeHeight = () => {
 		if (textarea && textarea.current) {
@@ -15,10 +20,29 @@ function Input() {
 	};
 	return (
 		<div className={styles.inputLine}>
+			{isImgInput ? <InputImgLine /> : null}
+			{isLocationInput ? <InputLocation /> : null}
 			<div className={styles.inputWrap}>
-				<img className={styles.icon} src="/icon/picture.svg" alt="" />
-				<img className={styles.icon} src="/icon/location.svg" alt="" />
+				<img
+					onClick={() => {
+						setIsLocationInput(false);
+						setIsImgInput(true);
+					}}
+					className={styles.icon}
+					src="/icon/picture.svg"
+					alt=""
+				/>
+				<img
+					onClick={() => {
+						setIsImgInput(false);
+						setIsLocationInput(true);
+					}}
+					className={styles.icon}
+					src="/icon/location.svg"
+					alt=""
+				/>
 				<textarea
+					value={inputValue}
 					onKeyUp={e => {
 						if (
 							e.shiftKey === false &&
@@ -26,24 +50,25 @@ function Input() {
 							e.currentTarget.value !== '\n' &&
 							e.currentTarget.value !== ''
 						) {
-							e.currentTarget.value = '';
 							setChatData(prev => [
 								...prev,
 								{
 									id: 4,
-									type: 'location',
-									chatContents: {x: '37.55467', y: '126.970609'},
+									type: 'text',
+									chatContents: e.currentTarget.value,
 									date: '2023. 5. 7 16:38',
 									checked: true,
 									isMy: true,
 								},
 							]);
+							setInputValue('');
 						}
 						handleResizeHeight();
 					}}
 					onInput={e => {
+						setInputValue(e.currentTarget.value);
 						if (e.currentTarget.value === '\n') {
-							e.currentTarget.value = '';
+							setInputValue('');
 						}
 						handleResizeHeight();
 					}}
@@ -51,7 +76,28 @@ function Input() {
 					onChange={handleResizeHeight}
 					ref={textarea}
 				/>
-				<img className={styles.icon} src="/icon/send.svg" alt="" />
+				<img
+					onClick={() => {
+						if (inputValue !== '\n' && inputValue !== '') {
+							setChatData(prev => [
+								...prev,
+								{
+									id: 4,
+									type: 'text',
+									chatContents: inputValue,
+									date: '2023. 5. 7 16:38',
+									checked: true,
+									isMy: true,
+								},
+							]);
+							setInputValue('');
+						}
+						handleResizeHeight();
+					}}
+					className={styles.icon}
+					src="/icon/send.svg"
+					alt=""
+				/>
 			</div>
 		</div>
 	);
--- components/header/alarm/alarmCard/chat/inputImgLine.tsx
+++ components/header/alarm/alarmCard/chat/inputImgLine.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from '@/styles/components/header/alarm/alarmCard/chat/inputImgLine.module.scss';
 
 function InputImgLine() {
-	return <div>I</div>;
+	return <div className={styles.imgLine}>cancel</div>;
 }
 
 export default InputImgLine;
