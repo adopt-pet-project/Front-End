397a4526e6e1a4fb2e01e8a8399f7bc1882c46b7
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2023-05-22 15:36:28 +0900

    Update carousel
    이미지 영역 터치시에도 스크롤 가능하게 수정

--- components/adopt/carousel.tsx
+++ components/adopt/carousel.tsx
@@ -36,7 +36,6 @@ export default function Carousel({
 	const touchMove = useCallback(
 		(e: TouchEvent) => {
 			if (!imagesRef.current || preventScroll.current) return;
-			e.preventDefault();
 			imagesRef.current.style.transform = `translateX(${
 				-1 * page
 			}00%) translate(${
