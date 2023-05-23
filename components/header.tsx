209af3f9da651156bed9064b6b2bb829162ebed3
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2023-05-24 01:39:58 +0900

    Update adopt, board
    게시글 작성 버튼 누를시 비 로그인 상태(토큰없음)검사 추가.

--- components/header.tsx
+++ components/header.tsx
@@ -3,44 +3,42 @@ import Search from './search/search';
 import styles from '@/styles/components/header.module.scss';
 import {AmodalWrap, AmodalType} from '@/utils/recoil/recoilStore';
 import {useRecoilState} from 'recoil';
+import {useRouter} from 'next/router';
 
 export default function Header({query, path}: {query: string; path: string}) {
 	const type = path === 'board' ? '게시판' : '분양';
 	const titleText = query ? `${query}에 대한 검색 결과` : type;
+	const router = useRouter();
 	const [modalRef, setModalRef] = useRecoilState(AmodalWrap);
 	const [modalType, setModalType] = useRecoilState(AmodalType);
 
-	function onClickNewAdopt() {
-		setModalType('setAdoptType');
-		modalRef!.current!.style.display = 'flex';
+	function onClickNewPost() {
+		if (!window.localStorage.getItem('accessToken')) {
+			window.dispatchEvent(new Event('fadeLogin'));
+			return;
+		}
+
+		if (path === 'board') {
+			router.push('/board/new');
+		} else {
+			setModalType('setAdoptType');
+			modalRef!.current!.style.display = 'flex';
+		}
 	}
 
 	return (
 		<div className={styles.header}>
 			<h1 className={styles.title}>{titleText}</h1>
 			<Search currentPath={path} />
-			{type === '게시판' && (
-				<Link className={styles.writeButton} href={`/${path}/new`}>
-					<img
-						style={{padding: '4px'}}
-						width={32}
-						height={32}
-						src="/icon/write.svg"
-						alt="write icon"
-					/>
-				</Link>
-			)}
-			{type === '분양' && (
-				<button onClick={onClickNewAdopt} className={styles.writeButton}>
-					<img
-						style={{padding: '4px'}}
-						width={32}
-						height={32}
-						src="/icon/write.svg"
-						alt="write icon"
-					/>
-				</button>
-			)}
+			<button onClick={onClickNewPost} className={styles.writeButton}>
+				<img
+					style={{padding: '4px'}}
+					width={32}
+					height={32}
+					src="/icon/write.svg"
+					alt="write icon"
+				/>
+			</button>
 		</div>
 	);
 }
