a77d76bf97da2f039589bd4d0cb385c517446611
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2023-05-23 19:46:18 +0900

    Update chat
    keyboard height 구하는 과정 삭제

--- components/chat/chatInput.tsx
+++ components/chat/chatInput.tsx
@@ -5,48 +5,15 @@ export default function ChatInput() {
 	const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 	const [currentMenuType, setCurrentMenuType] = useState<number>(0);
 
-	// const keyboardHeight = useRef<number>(180);
 	const inputRef = useRef<HTMLInputElement>(null);
 
-	const initViewport = useRef<Viewport>();
-	const keyboardHeight = useRef<number>(0);
-
-	function detectKeyboardHeight() {
-		// if (
-		// 	initViewport.current?.width === window.innerWidth &&
-		// 	initViewport.current.height - window.innerHeight > 50 &&
-		// 	initViewport.current.height - window.innerHeight < 200
-		// ) {
-		// 	// resize 시 width 가 동일하면서도 height 가 50 ~ 200 사이 변화하면 키보드 높이로 취급하자.
-		// 	keyboardHeight.current = initViewport.current.height - window.innerHeight;
-		// } else {
-		// 	// 일반적인 resize 라면 initViewport 갱신해 keyboard height 가 설정되는것을 방지
-		// 	initViewport.current!.height = window.innerHeight;
-		// }
-		keyboardHeight.current = window.innerHeight;
-		setCurrentMenuType(Date.now());
-	}
-
-	useEffect(() => {
-		initViewport.current = {
-			width: window.innerWidth,
-			height: window.innerHeight,
-		};
-
-		window.addEventListener('resize', detectKeyboardHeight);
-
-		return () => {
-			window.removeEventListener('resize', detectKeyboardHeight);
-		};
-	}, []);
-
-	function onClickOpen() {
-		if (inputRef.current) inputRef.current.blur();
-		setIsMenuOpen(true);
-	}
-
-	function onClickClose() {
-		setIsMenuOpen(false);
+	function onClickMenu() {
+		if (!isMenuOpen) {
+			if (inputRef.current) inputRef.current.blur();
+			setIsMenuOpen(true);
+		} else {
+			setIsMenuOpen(false);
+		}
 	}
 
 	function onFocusInput(e: BaseSyntheticEvent) {
@@ -57,28 +24,17 @@ export default function ChatInput() {
 
 	return (
 		<div className={styles.container}>
-			for moblie keyboard height test, innerHeight : {keyboardHeight.current}
 			<div className={styles.chatInput}>
-				{isMenuOpen ? (
-					<img
-						style={{
-							transform: 'rotate(45deg)',
-						}}
-						src="/icon/plus.svg"
-						width={20}
-						height={20}
-						alt="close"
-						onClick={onClickClose}
-					/>
-				) : (
-					<img
-						src="/icon/plus.svg"
-						width={20}
-						height={20}
-						alt="open"
-						onClick={onClickOpen}
-					/>
-				)}
+				<img
+					style={{
+						transform: `${isMenuOpen ? 'rotate(45deg)' : 'rotate(0deg)'}`,
+					}}
+					src="/icon/plus.svg"
+					width={22}
+					height={22}
+					alt="menu"
+					onClick={onClickMenu}
+				/>
 				<input
 					type="text"
 					placeholder="메시지를 입력하세요."
@@ -93,7 +49,32 @@ export default function ChatInput() {
 					onClick={onClickSend}
 				/>
 			</div>
-			{isMenuOpen && <div className={styles.menu}></div>}
+			{isMenuOpen && (
+				<ul className={styles.menu}>
+					<li>
+						<div className={styles.icon}>
+							<img
+								src="/icon/picture.svg"
+								alt="picture"
+								width={28}
+								height={28}
+							/>
+						</div>
+						사진
+					</li>
+					<li>
+						<div className={styles.icon}>
+							<img
+								src="/icon/location.svg"
+								alt="picture"
+								width={24}
+								height={24}
+							/>
+						</div>
+						위치
+					</li>
+				</ul>
+			)}
 		</div>
 	);
 }
