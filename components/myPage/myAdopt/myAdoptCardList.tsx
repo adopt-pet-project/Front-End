47af9764b2dfbed01e84f2f8f5bda4f6b0e74159
ikhyeons <skantrkwl789@naver.com>
2023-05-28 00:19:22 +0900

    Update myAdopt
    내 분양 정보 가져오기

--- components/myPage/myAdopt/myAdoptCardList.tsx
+++ components/myPage/myAdopt/myAdoptCardList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useRecoilState} from 'recoil';
 import {AmyAdoptBoardType, AmyAdoptModal} from '@/utils/recoil/recoilStore';
 import MyAdoptModal from './myAdoptModal';
@@ -6,30 +6,40 @@ import MyAdoptCard from './myAdoptCard';
 import styles from '@/styles/components/myPage/myAdopt/myAdoptCardList.module.scss';
 
 function MyAdoptCardList() {
-	const DummyData: any[] = [];
 	const [myAdoptBoardType, setMyAdoptBoardType] =
 		useRecoilState(AmyAdoptBoardType);
 	const [myAdoptModal, setMyAdoptModal] = useRecoilState(AmyAdoptModal);
+	const [accessToken, setAccessToken] = useState(
+		typeof window !== 'undefined' ? localStorage.getItem('accessToken') : '',
+	);
+	const [myAdoptData, setMyAdoptData] = useState<any[]>([]);
 
-	for (let i = 0; i < 9; i++) {
-		DummyData.push({
-			id: i,
-			title: '타이틀',
-			context: '본문',
-			author: '김성태',
-			view: 3,
-			comment: 6,
-			like: 2,
-			publishedAt: '글쓴시간',
-			thumb:
-				'https://mblogthumb-phinf.pstatic.net/MjAxNjExMjJfMjEx/MDAxNDc5NzQ0MDAzOTQy.-ax_EfCGWODogkXHIuDpovF5XHfaYi_s8EtRVWEjYXQg.R4kQWRtNC7pNxF03-aKWylWpGoRgE7vGDeagJm7Sgk0g.PNG.outdoor-interlaken/%EC%8A%A4%EC%9C%84%EC%8A%A4_%EC%97%AC%ED%96%89%ED%95%98%EA%B8%B0_%EC%A2%8B%EC%9D%80_%EA%B3%84%EC%A0%88_christofs70.png?type=w800',
-		});
-	}
+	useEffect(() => {
+		async function fetchMyInfo() {
+			await fetch(
+				`${process.env.NEXT_PUBLIC_SERVER_URL}/mypage/adopt?status=${myAdoptBoardType}`,
+				{
+					method: 'GET',
+					headers: {
+						Authorization: `${accessToken}`,
+					},
+				},
+			)
+				.then(response => response.json())
+				.then(data => {
+					data.status === 401
+						? console.log('무효한 토큰')
+						: setMyAdoptData(data);
+					data.status === 500 ? alert('DB오류') : null;
+				});
+		}
+		fetchMyInfo();
+	}, [myAdoptBoardType]);
 
 	return (
 		<>
 			<ul className={styles.cardList}>
-				{DummyData.map((data, i) => (
+				{myAdoptData.map((data, i) => (
 					<MyAdoptCard key={i} boardType={myAdoptBoardType} article={data} />
 				))}
 			</ul>
