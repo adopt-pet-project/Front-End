1e8eb36b3c99c78799741b466f27bcde5a1e39d6
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2023-05-29 02:42:56 +0900

    Update chat
    사진 좌표 추가.
    시간과 날짜 표시방법 변경.

--- components/chat/flightMessageArea.tsx
+++ components/chat/flightMessageArea.tsx
@@ -1,5 +1,7 @@
 import styles from '@/styles/components/chat/flightMessageArea.module.scss';
 
+// 전송후 응답 안받은 메시지 표시해주는 Component
+
 export default function FlightMessageArea({message}: {message: FlightChat[]}) {
 	return (
 		<div className={styles.container}>
--- components/chat/newMessageArea.tsx
+++ components/chat/newMessageArea.tsx
@@ -1,8 +1,8 @@
+import {Map, MapMarker, useInjectKakaoMapApi} from 'react-kakao-maps-sdk';
 import styles from '@/styles/components/chat/messageArea.module.scss';
 
 const profile =
 	'https://project-adopt-bucket.s3.ap-northeast-2.amazonaws.com/other/default-profile-image.jpeg';
-
 export default function NewMessageArea({
 	message,
 	mine,
@@ -12,15 +12,32 @@ export default function NewMessageArea({
 	mine: boolean;
 	authorId: number;
 }) {
+	console.log(message);
+
 	return (
 		<div className={styles.container}>
 			{message.map((chat: Chat, index: number) => {
 				chat.mine = chat.senderNo === authorId ? mine : !mine;
-				const dateString =
-					index === 0 ||
-					message[index].dateString != message[index - 1].dateString
-						? `${chat.dateString} ${chat.timeString}`
-						: `${chat.timeString}`;
+				let dateString = '';
+				let coords =
+					chat.contentType === 'coords'
+						? chat.content.split(' ').map(Number)
+						: null;
+
+				if (index === message.length - 1) {
+					dateString = `${chat.timeString}`;
+				} else if (index === 0) {
+					dateString = `${chat.dateString} ${chat.timeString}`;
+				} else {
+					if (message[index].dateString != message[index - 1].dateString)
+						dateString += message[index].dateString;
+
+					if (dateString != '') dateString += ' ';
+
+					if (message[index].timeString != message[index + 1].timeString)
+						dateString += message[index].timeString;
+				}
+
 				return (
 					<div
 						key={chat.id}
@@ -33,7 +50,47 @@ export default function NewMessageArea({
 								chat.mine ? styles.rowReverse : ''
 							}`}
 						>
-							<span className={styles.text}>{chat.content}</span>
+							{chat.contentType === 'coords' && (
+								<div className={styles.multimedia}>
+									<img
+										style={{
+											width: 'calc(100% - 12px)',
+											height: 'calc(100% - 12px)',
+											margin: '6px',
+											borderRadius: '8px',
+										}}
+										src={chat.content}
+									/>
+								</div>
+							)}
+
+							{chat.contentType === 'picture' && (
+								<div className={styles.multimedia}>
+									<Map
+										style={{
+											width: 'calc(100% - 12px)',
+											height: 'calc(100% - 12px)',
+											margin: '6px',
+											borderRadius: '8px',
+										}}
+										center={{
+											lat: (coords as number[])[0],
+											lng: (coords as number[])[1],
+										}}
+										level={3}
+									>
+										<MapMarker
+											position={{
+												lat: (coords as number[])[0],
+												lng: (coords as number[])[1],
+											}}
+										/>
+									</Map>
+								</div>
+							)}
+							{(chat.contentType === 'text' || chat.contentType == null) && (
+								<span className={styles.text}>{chat.content}</span>
+							)}
 							<span className={styles.time}>{dateString}</span>
 						</div>
 						{(index === 0 ||
