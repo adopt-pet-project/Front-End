0d952b0d3312afd100782d6d795e141ad0ee2646
ikhyeons <skantrkwl789@naver.com>
2023-05-30 21:45:55 +0900

    Update activity
    활동 내역 api 연결

--- components/activity/activityCardList.tsx
+++ components/activity/activityCardList.tsx
@@ -1,43 +1,63 @@
 import {useRecoilState} from 'recoil';
+import {useEffect, useState} from 'react';
 import {AisComment} from '@/utils/recoil/recoilStore';
 import ActivityCard from './activityCard';
 import ActivityComment from './activityComment';
 import styles from '@/styles/components/activity/activityCardList.module.scss';
+import useRefreshToken from '@/utils/hooks/useRefreshToken';
 
 function ActivityCardList() {
-	const DummyDataDoc: any[] = [];
-	const DummyDataComment: any[] = [];
+	const [accessToken, setAccessToken] = useState(
+		typeof window !== 'undefined' ? localStorage.getItem('accessToken') : '',
+	);
 	const [isComment, setIsComment] = useRecoilState(AisComment);
+	const [myDoc, setMyDoc] = useState<Activitydoc[]>([]);
+	const [myComment, setMyComment] = useState<Activitycomment[]>([]);
+	const refresh = useRefreshToken();
 
-	for (let i = 0; i < 9; i++) {
-		DummyDataDoc.push({
-			id: i,
-			title: '타이틀',
-			context: '본문',
-			author: '김성태',
-			view: 3,
-			comment: 6,
-			like: 2,
-			publishedAt: '글쓴시간',
-			thumb:
-				'https://mblogthumb-phinf.pstatic.net/MjAxNjExMjJfMjEx/MDAxNDc5NzQ0MDAzOTQy.-ax_EfCGWODogkXHIuDpovF5XHfaYi_s8EtRVWEjYXQg.R4kQWRtNC7pNxF03-aKWylWpGoRgE7vGDeagJm7Sgk0g.PNG.outdoor-interlaken/%EC%8A%A4%EC%9C%84%EC%8A%A4_%EC%97%AC%ED%96%89%ED%95%98%EA%B8%B0_%EC%A2%8B%EC%9D%80_%EA%B3%84%EC%A0%88_christofs70.png?type=w800',
-		});
-	}
+	useEffect(() => {
+		// 유저 게시글 조회
+		async function getMyDoc() {
+			let URL = `${process.env.NEXT_PUBLIC_SERVER_URL}/mypage/article`;
+			let response = await fetch(`${URL}`, {
+				method: 'GET',
+				headers: {
+					Authorization: `${accessToken}`,
+				},
+			});
+			const result = await response.json();
+			console.log(result);
+			if (result.status === 401) refresh();
+			setMyDoc(await result);
+		}
 
-	for (let i = 0; i < 9; i++) {
-		DummyDataComment.push({
-			id: i,
-			docId: i,
-			title: '라면 쏟음...',
-			comment: '어쩌긴요 버려야죠',
-			publishedAt: '댓글단시간',
-		});
-	}
+		getMyDoc();
+	}, []);
+
+	useEffect(() => {
+		// 유저 댓글 조회
+		async function getMyComment() {
+			let URL = `${process.env.NEXT_PUBLIC_SERVER_URL}/mypage/comment`;
+			let response = await fetch(`${URL}`, {
+				method: 'GET',
+				headers: {
+					Authorization: `${accessToken}`,
+				},
+			});
+			const result = await response.json();
+			console.log(result);
+			if (result.status === 401) refresh();
+			setMyComment(await result);
+		}
+
+		getMyComment();
+	}, []);
+
 	return (
 		<ul className={styles.cardList}>
 			{isComment === false
-				? DummyDataDoc.map((data, i) => <ActivityCard key={i} article={data} />)
-				: DummyDataComment.map((data, i) => (
+				? myDoc.map((data, i) => <ActivityCard key={i} article={data} />)
+				: myComment.map((data, i) => (
 						<ActivityComment key={i} article={data} />
 				  ))}
 		</ul>
