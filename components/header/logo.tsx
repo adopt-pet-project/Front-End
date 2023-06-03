4b02570039c2e946ccc6a46a97bd431f0614cbef
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2023-06-03 17:31:08 +0900

    Add logo

--- components/header/logo.tsx
+++ components/header/logo.tsx
@@ -1,5 +1,9 @@
 import styles from '@/styles/components/header/header.module.scss';
 
 export default function Logo() {
-	return <div>Logo</div>;
+	return (
+		<div>
+			<img src="logo.png" alt="logo" height={52} />
+		</div>
+	);
 }
