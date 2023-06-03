543f28a77e131107d2b02a1392a607ded53e87ec
Surplus05 <104773096+Surplus05@users.noreply.github.com>
2023-06-03 11:11:17 +0900

    Update letterModal.tsx

--- components/letterModal.tsx
+++ components/letterModal.tsx
@@ -16,6 +16,10 @@ export default function LetterModal() {
 	async function onClickApply() {
 		if (!window.localStorage.getItem('accessToken')) {
 			dispatchEvent(new Event('fadeLogin'));
+			textareaRef.current!.value = '';
+			if (modalRef && modalRef.current!)
+				modalRef.current.style.display = 'none';
+
 			return;
 		}
 
@@ -33,11 +37,6 @@ export default function LetterModal() {
 				}),
 			},
 		);
-
-		console.log({
-			targetId: letterTarget.targetId,
-			contents: textareaRef.current?.value,
-		});
 
 		textareaRef.current!.value = '';
 		if (modalRef && modalRef.current!) modalRef.current.style.display = 'none';
